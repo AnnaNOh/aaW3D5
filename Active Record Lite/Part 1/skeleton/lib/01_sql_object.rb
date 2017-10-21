8f49c8c1193ec4c85ed073ec2ee1a8eb2aa7165c
Anna Oh <annaoh1994@gmail.com>
2017-10-20 17:04:12 -0700

    Finish up to section 2

--- Active Record Lite/Part 1/skeleton/lib/01_sql_object.rb
+++ Active Record Lite/Part 1/skeleton/lib/01_sql_object.rb
@@ -3,6 +3,7 @@ require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
+
 class SQLObject
   def self.columns
     if @columns == nil
@@ -19,36 +20,70 @@ class SQLObject
     @columns
   end
 
-  def self.finalize!
-    columns.each do |col|
 
+  def self.finalize!
+    self.columns.each do |col|
+      define_method(col) do
+        attributes[col]
+      end
+
+      setter_method_name = col.to_s + "="
+      define_method(setter_method_name) do |value|
+        attributes[col] = value
+      end
     end
   end
+
 
   def self.table_name=(table_name)
     @table_name = table_name
   end
+
 
   def self.table_name
     class_name = self.to_s.downcase
     @table_name = "#{class_name}s"
   end
 
+
   def self.all
-    # ...
+    data = DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM #{table_name}
+    SQL
+    parse_all(data)
   end
+
 
   def self.parse_all(results)
-    # ...
+    result = []
+    results.each_with_index do |el, i|
+      result << self.new(results[i])
+    end
+    result
   end
+
 
   def self.find(id)
-    # ...
+    all.each do |el|
+      if el.id == id
+        return el
+      end
+    end
+    nil
   end
 
+
   def initialize(params = {})
-    # ...
+    params.each do |key, value|
+      if self.class.columns.include?(key.to_sym)
+        send("#{key}=", value)
+      else
+        raise "unknown attribute '#{key}'"
+      end
+    end
   end
+
 
   def attributes
     if @attributes.nil?
@@ -57,19 +92,45 @@ class SQLObject
     @attributes
   end
 
+
   def attribute_values
-    # ...
+    @attributes.values
   end
+
 
   def insert
-    # ...
+    cols = self.class.columns[1..-1]
+    col_names = cols.join(",")
+    question_marks = (["?"]*(cols.length)).join(",")
+
+    data = DBConnection.execute(<<-SQL, attribute_values)
+      INSERT INTO #{self.class.table_name} (#{col_names})
+      VALUES (#{question_marks})
+    SQL
+
+    @attributes[:id] = DBConnection.last_insert_row_id
   end
+
 
   def update
-    # ...
+    cols = self.class.columns[1..-1]
+    p col_names = cols.join(" = ?, ") + " = ?"
+    p attribute_values
+
+    data = DBConnection.execute(<<-SQL, attribute_values[1..-1], attribute_values[0])
+      UPDATE #{self.class.table_name}
+      SET #{col_names}
+      WHERE id = ?
+    SQL
   end
 
+
+
   def save
-    # ...
+    if self.id.nil?
+      insert
+    else
+      update
+    end
   end
 end
--- Active Record Lite/Part 1/skeleton/lib/02_searchable.rb
+++ Active Record Lite/Part 1/skeleton/lib/02_searchable.rb
@@ -3,10 +3,19 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    keys = params.keys.join(" = ? AND ") + " = ?"
+    values = params.values
+
+    data = DBConnection.execute(<<-SQL, values)
+      SELECT  *
+      FROM  #{self.table_name}
+      WHERE #{keys}
+    SQL
+    
+    parse_all(data)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
